0d89158fda1b8e0a99c570b7dd7e4c9ff00fe504
Jared-young <54957290+Jared-young@users.noreply.github.com>
2023-09-17 09:54:50 -0400

    Intro to JS

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,34 @@
+//All different types of alerts
+//alert("Hello World!");
+//window.alert("Hello World!");
+//document.write('Hello, World!');
+
+//var A = "This is a string";
+//document.write(A);
+//window.alert(A);
+
+//document.write("Lisa told Bart, \"Knock it off, Bart! or I'll tell dad!\"<br>\"Eat my shorts!\" Bart Responded.");
+
+//document.write("\"Be who you are and say what you feel,"
+//+ " because those who mind don\'t matter and those who matter don\'t mind.\""
+//+" -Dr. Seuss");
+
+//var B = "\"Be who you are and say what you feel,"
+//+ " because those who mind don\'t matter and those who matter don\'t mind.\""
+//+" -Dr. Seuss" + "Lisa told Bart, \"Knock it off, Bart! or I'll tell dad!\"<br>\"Eat my shorts!\" Bart Responded.";
+//document.write(B);
+
+//var Family = "the Arezzinis", Dad = "Jeremiah", Mom = "Hermoine", Daughter = "Penny", Son = "Zorro"
+//document.write(Dad)
+
+//This Expression will multiply 4 and 3 and display the answer on a webpage
+//document.write(4*3);
+
+//Two Variables are being created
+var Sent1 = "This is the beginning of the string";
+var Sent2 = " and this is the end of this string."
+var Sent3 = " This is the value of an expression: "
+//concatenate and write those variables to the webpage
+document.write(Sent1 + Sent2 + Sent3);
+//write an expression to the webpage at the end of the sting. 
+document.write(4*3+7);
