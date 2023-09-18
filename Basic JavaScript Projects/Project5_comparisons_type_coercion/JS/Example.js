0532584211c909720a69f5d4770b8f844c665566
Jared-young <54957290+Jared-young@users.noreply.github.com>
2023-09-18 17:31:26 -0400

    small updates

--- Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/Example.js
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/Example.js
@@ -6,17 +6,21 @@ function my_Function() {
     document.getElementById("Test").innerHTML = isNaN('007');
 }
 
+//Infinity and negative infinity
 document.write(2E310);
 document.write(-3E310);
 
+//is 10>2 is 10<2
 document.write(10>2);
 document.write(10<2);
 
+//preforms calculations in console
 console.log(2+2);
 console.log(2<2);
 
-//document.write(10 == 10);
-//document.write(10 = 2);
+//is 10 = 10 or is 10 = 2
+document.write(10 == 10);
+document.write(10 == 2);
 
 X = 10;
 Y = 10;
