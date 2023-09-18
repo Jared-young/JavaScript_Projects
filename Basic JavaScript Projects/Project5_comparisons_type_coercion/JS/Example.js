3d5ba277a45c5a3a91007fbde941f9277ae41540
Jared-young <54957290+Jared-young@users.noreply.github.com>
2023-09-18 18:12:28 -0400

    pushed

--- Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/Example.js
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/Example.js
@@ -50,6 +50,10 @@ function not_Function() {
     document.getElementById("Not").innerHTML = !(20>10);
 }
 
-function not_Function() {
-    document.getElementById("Not").innerHTML = !(5>10);
+function not_Function2() {
+    document.getElementById("Not2").innerHTML = !(5>10);
 }
+
+//type of operators
+document.write(typeof "Word");
+document.write(typeof 3);
